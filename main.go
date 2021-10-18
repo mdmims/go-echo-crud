7ecadfb67c4a62c3468b2785cae416fe18a0b49e
michael mims <michael.mims@verizon.com>
2021-10-18 12:05:29 -0400

    init, proj structure, godotenv

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
+	}
+}
+
+func main() {
+	panic("not implemented")
+}
